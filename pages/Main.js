ecd628b86af105cea5158f5dcf8db0219740c924
psh960831 <psh960831@gmail.com>
2022-09-06 21:24:28 +0900

    2022-09-06

---
+++ pages/Main.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./Main.css";
+import App from "../src/App";
+
+
+function Main () {
+    return(
+        <main className="home_content">
+            <div className="intro">
+                <div className="intro_info">
+                    <div className="main_info">방탈출 어때?</div>
+                    <div className="sub_info">방탈출 어딘지 찾아보자</div>
+                </div>
+            </div>
+        </main>
+    );
+
+}
+export default App;
